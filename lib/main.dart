03834e9b249f9b4f03efec827f70b501f5164db4
6510615112 <6510615112@student.tu.ac.th>
2025-04-17 06:23:11 +0700

    merge with tui branch and delete auth_layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,12 @@ import 'package:authenticatu/Screen/login.dart';
 import 'package:authenticatu/database/key_db.dart';
 import 'package:authenticatu/providers/otp_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MaterialApp(home: LoginScreen()));
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await TOTPDB.initialize();
-
   runApp(
     MultiProvider(
       providers: [
